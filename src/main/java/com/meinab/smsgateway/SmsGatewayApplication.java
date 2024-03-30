99c6e0957723aab3cb5eef2323654658f6fc2426
Abinet <abinettamiru28@gmail.com>
2024-03-30 22:38:27 +0300

    feat: add feature to accept port and server address from cli during the initialization of the jar

--- src/main/java/com/meinab/smsgateway/SmsGatewayApplication.java
+++ src/main/java/com/meinab/smsgateway/SmsGatewayApplication.java
@@ -7,6 +7,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SmsGatewayApplication {
 
     public static void main(String[] args) {
+        String defaultServerAddress = "127.0.0.1";
+        int defaultPort = 8080;
+
+        String serverAddress = args.length > 0 ? args[0] : defaultServerAddress;
+        int port = args.length > 1 ? Integer.parseInt(args[1]) : defaultPort;
+
+        System.setProperty("server.address", serverAddress);
+        System.setProperty("server.port", String.valueOf(port));
+
         SpringApplication.run(SmsGatewayApplication.class, args);
     }
 
