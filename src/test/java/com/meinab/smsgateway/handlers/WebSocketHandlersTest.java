86454d575ff4f893f6277a2214c8003a9034ec9c
Abinet <abinettamiru28@gmail.com>
2024-03-31 13:44:00 +0300

    feat: add test for retrives userId and stire session by user id

--- src/test/java/com/meinab/smsgateway/handlers/WebSocketHandlersTest.java
+++ src/test/java/com/meinab/smsgateway/handlers/WebSocketHandlersTest.java
@@ -1,7 +1,10 @@
 package com.meinab.smsgateway.handlers;
 
 import com.meinab.smsgateway.exceptions.UserNotFound;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -9,7 +12,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
+
 import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
@@ -23,37 +28,57 @@ class WebSocketHandlersTest {
     @InjectMocks
     private WebSocketHandlers handlers;
 
+    private void setupTestSession(String userId) throws Exception {
+        when(mockSession.getAttributes()).thenReturn(Map.of("userId", userId));
+        handlers.afterConnectionEstablished(mockSession);
+    }
+
     @Test
     @DisplayName("when the connection established")
-    @Order(2)
-    void testAfterConnectionEstablished() throws Exception {
-        when(mockSession.getAttributes()).thenReturn(Map.of("userId","testUser"));
-        handlers.afterConnectionEstablished(mockSession);
-        assertEquals(mockSession, handlers.getSessionByUserId("testUser"));
+    void testAfterConnectionEstablished_SendWelcomeMessage() throws Exception {
+        setupTestSession("testUser");
+
         verify(mockSession, times(1)).sendMessage(any(TextMessage.class));
     }
 
     @Test
+    @DisplayName("Retrieves user ID from session attributes")
+    void testAfterConnectionEstablished_RetrievesUserId() throws Exception {
+        setupTestSession("testUser");
+
+        WebSocketSession retrievedUserId = handlers.getSession((String) mockSession.getAttributes().get("userId"));
+        assertEquals("testUser", retrievedUserId.getAttributes().get("userId"));
+    }
+
+    @Test
+    @DisplayName("Stores session in map by user ID")
+    void testAfterConnectionEstablished_StoresSessionByUserId() throws Exception {
+        setupTestSession("testUser");
+
+        assertEquals(mockSession, handlers.getSessionByUserId("testUser"));
+    }
+
+    @Test
     @DisplayName("get session by id for existing user")
-    @Order(3)
-    void testGetSessionByUserIdForTheExistingUser(){
-        assertDoesNotThrow(()->handlers.getSessionByUserId("testUser"));
+    void testGetSessionByUserIdForTheExistingUser() throws Exception {
+        setupTestSession("testUser");
+
+        assertDoesNotThrow(() -> handlers.getSessionByUserId("testUser"));
     }
 
     @Test
     @DisplayName("get session by id for non-existing user")
-    @Order(1)
-    void testGetSessionByUserIdWhenForNewUser(){
-        assertThrows(UserNotFound.class,()->handlers.getSessionByUserId("testUser"));
+    void testGetSessionByUserIdWhenForNewUser() {
+        assertThrows(UserNotFound.class, () -> handlers.getSessionByUserId("nonExistingUser"));
     }
 
     @Test
     @DisplayName("when the connection closed")
-    @Order(4)
     void testAfterConnectionClosed() throws Exception {
-        when(mockSession.getAttributes()).thenReturn(Map.of("userId","testUser"));
-        handlers.afterConnectionClosed(mockSession,closeStatus);
-        assertThrows(UserNotFound.class,()->handlers.getSessionByUserId("testUser"));
+        setupTestSession("testUser");
+
+        handlers.afterConnectionClosed(mockSession, closeStatus);
+        assertThrows(UserNotFound.class, () -> handlers.getSessionByUserId("testUser"), "Session for the user still exists and it is not closed");
     }
 
 
