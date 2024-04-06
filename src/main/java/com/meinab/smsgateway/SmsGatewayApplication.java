87879283b57f95922fe64923abc95d2f6e70b4eb
Abinet <abinettamiru28@gmail.com>
2024-04-06 17:56:54 +0300

    refactor: refactor SmsGatewayApplication to make it testable

--- src/main/java/com/meinab/smsgateway/SmsGatewayApplication.java
+++ src/main/java/com/meinab/smsgateway/SmsGatewayApplication.java
@@ -7,16 +7,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SmsGatewayApplication {
 
     public static void main(String[] args) {
+        configureApplication(args);
+        SpringApplication.run(SmsGatewayApplication.class, args);
+    }
+    static void configureApplication(String[] args) {
         String defaultServerAddress = "127.0.0.1";
         int defaultPort = 8080;
-
         String serverAddress = args.length > 0 ? args[0] : defaultServerAddress;
         int port = args.length > 1 ? Integer.parseInt(args[1]) : defaultPort;
 
         System.setProperty("server.address", serverAddress);
         System.setProperty("server.port", String.valueOf(port));
-
-        SpringApplication.run(SmsGatewayApplication.class, args);
     }
 
 }
