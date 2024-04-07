7d8a3154a5d6b236faa1f326e261bc04d58812c9
Abinet <abinettamiru28@gmail.com>
2024-04-07 20:59:33 +0300

    feat: add test for MessageSender class

--- src/test/java/com/meinab/smsgateway/services/MessageSenderTest.java
+++ src/test/java/com/meinab/smsgateway/services/MessageSenderTest.java
@@ -1,5 +1,6 @@
 package com.meinab.smsgateway.services;
 
+import com.meinab.smsgateway.exceptions.UserNotFound;
 import com.meinab.smsgateway.handlers.WebSocketHandlers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -23,13 +24,24 @@ class MessageSenderTest {
     private MessageSender messageSender;
 
     @Test
-    @DisplayName("send message")
+    @DisplayName("Given a username and message, when the session is open, it sends the message to the user")
     void testSendMessage() throws Exception {
         when(mockHandler.getSessionByUserId("testUser")).thenReturn(mockWebSocketSession);
         when(mockHandler.getSessionByUserId("testUser").isOpen()).thenReturn(true);
 
         messageSender.sendMessage("testUser","test 1,2,3");
         verify(mockWebSocketSession).sendMessage(new TextMessage("test 1,2,3"));
+
+    }
+
+    @Test
+    @DisplayName("Given a username and message, when the session is not open, it throws a UserNotFound exception")
+    void testSendMessageWhenItFails() {
+        when(mockHandler.getSessionByUserId("testUser")).thenReturn(mockWebSocketSession);
+        when(mockHandler.getSessionByUserId("testUser").isOpen()).thenReturn(false);
+
+        assertThrows(UserNotFound.class,()->messageSender.sendMessage("testUser","test 1, 2, 3"));
+
 
     }
 }
